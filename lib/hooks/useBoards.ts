f6568c7d7dd3a02388da7b6d6e8831ca4fcdbdb9
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-09-15 16:17:05 +0700

    handle load boards data when go to Dashboard page, update UI on Dashboard page

--- lib/hooks/useBoards.ts
+++ lib/hooks/useBoards.ts
@@ -1,7 +1,7 @@
 "use client";
 import { useUser } from "@clerk/nextjs";
-import { boardDataService } from "../services";
-import { useState } from "react";
+import { boardDataService, boardService } from "../services";
+import { useEffect, useState } from "react";
 import { Board } from "../supabase/models";
 import { useSupabase } from "../supabase/SupabaseProvider";
 
@@ -11,6 +11,27 @@ export function useBoards() {
   const [boards, setBoards] = useState<Board[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (user) loadBoards();
+  }, [user, supabase]);
+
+  async function loadBoards() {
+    if (!user) return;
+
+    try {
+      setLoading(true);
+      setError(null);
+      const data = await boardService.getBoards(supabase!, user.id);
+      setBoards(data);
+    } catch (error) {
+      setError(
+        error instanceof Error ? error.message : "Failed to load boards."
+      );
+    } finally {
+      setLoading(false);
+    }
+  }
 
   async function createBoard(boardData: {
     title: string;
